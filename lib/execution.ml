7b2d7abb9a194da0aef34c233854cbdfe040c986
Rellikeht <michaelhemperek@gmail.com>
2025-01-18 10:43:33 +0100

    add clauses to execution

--- lib/execution.ml
+++ lib/execution.ml
@@ -4,72 +4,72 @@ open Base
 
 exception Unimplemented
 
-let rec exec_exp (e : exp) =
+let rec parse_table (state : state) (table : field list) = raise Unimplemented
+
+and exec_exp (state : state) (e : exp) =
   match e with
-  | Nil -> Value Nil
+  | Nil | Vararg -> Value Nil
   | False -> Value False
   | True -> Value True
   | Number n -> Value (Number n)
-  | Vararg -> raise (Invalid_argument "Vararg")
-  | _ -> Value Nil
+  | String s -> Value (String s)
+  | Table t -> Value (Table (parse_table state t))
+  (* | Func f -> call_func s f *)
+  | _ -> raise Unimplemented
 
-and call_builtin (f : builtin_func) (s : state) (a : exp list) =
-  let args = List.map ~f:exec_exp a in
-  f s args
+and call_builtin (func : builtin_func) (state : state) (args : exp list) =
+  let args = List.map ~f:(exec_exp state) args in
+  func state args
 
-and call_func (s : state) (funcall : function_call) =
+and call_func (state : state) (funcall : function_call) =
   match funcall with
   (* *)
   | Function (var, args) -> (
       match var with
       (* *)
       | Named name -> (
-          match Hashtbl.find s.symbols name with
-          | None -> () (* TODO raise *)
-          | Some (Builtin f) -> call_builtin f s args
+          match Hashtbl.find state.symbols name with
+          | None -> raise Unimplemented (* TODO raise *)
+          | Some (Builtin f) -> call_builtin f state args
           | _ -> ())
       | _ -> ())
   | _ -> ()
 
-and exec_last (s : state) = function
+and exec_last (state : state) = function
   (* *)
   | Break -> raise Unimplemented
   | Return _ -> raise Unimplemented
 
-and exec_statement (state : state) = function
+and exec_statement (state : state) (stmt : statement) =
+  match stmt with
   | Assignment (names, vals) -> raise Unimplemented
   | Call funcall -> call_func state funcall
-  (* | Do block -> ( *)
-  (*     (1* *1) *)
-  (*     match block with *)
-  (*     | Statements statements -> exec_chunk state statements None *)
-  (*     | Ended (statements, last) -> exec_chunk state statements (Some last)) *)
-  (* | While of exp * block *)
-  (* | Repeat of block * exp *)
-  (* | If of exp * block * elseif list * block option *)
+  | Do block -> exec_chunk state block
+  | While (cond, body) -> raise Unimplemented
+  | Repeat (body, cond) -> raise Unimplemented
+  | If (cond, body, elseifs, elseblock) -> raise Unimplemented
   | For (name, start, last, step, body) -> raise Unimplemented
   | ForIn (names, exps, body) -> raise Unimplemented
   | Function (name, body) -> add_function state name body
   | LocalFunction (name, funcbody) ->
       update_locals state [ name ] [ Func funcbody ]
   | Local (names, exps) -> update_locals state names exps
-  | _ -> ()
 
-and exec_statements (state : state) (ss : statement list)
+and exec_statements (state : state) (stmts : statement list)
     (last : last_statement option) =
-  match ss with
+  match stmts with
   | statement :: rest ->
       exec_statement state statement;
       exec_statements state rest last
-  (* | [] -> begin match last with *)
-  (*   | None -> () *)
-  (*   | Some last -> exec_last state last *)
-  (* end *)
-  | [] -> Option.bind last ~f:(fun x -> Some (exec_last state x))
+  | [] -> (
+      match last with
+      (* *)
+      | None -> ()
+      | Some last -> exec_last state last)
 
 and exec_chunk (state : state) = function
-  | Statements ss -> exec_statements state ss None
-  | Ended (ss, e) -> exec_statements state ss (Some e)
+  | Statements stmts -> exec_statements state stmts None
+  | Ended (stmts, last) -> exec_statements state stmts (Some last)
 
 and exec_block s c = exec_chunk s c
 
